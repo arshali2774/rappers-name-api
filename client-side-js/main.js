99488cc014c7ff10e6020d1487f0a1248cb3de80
arshali2774 <56296638+arshali2774@users.noreply.github.com>
2023-02-16 15:58:02 +0530

    changed the main js

--- client-side-js/main.js
+++ client-side-js/main.js
@@ -1,1 +1,15 @@
-alert("woe");
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const rapperName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://rappers-name.onrender.com/api/${rapperName}`
+    );
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("h2").innerText = data.birthname;
+  } catch (err) {
+    console.log(err);
+  }
+}
